8f74d7e2e3bacd589b90f1336b10a2a70f36dd61
Alysson Miguel <Edicao.Dash@gmail.com>
2024-07-10 19:35:48 -0300

    Navbar

---
+++ project/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils"
+import "./globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: "E-commerce",
+  description: "Projeto alinhado para criação de um e-commerce",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}>
+        {children}
+        </body>
+    </html>
+  );
+}
