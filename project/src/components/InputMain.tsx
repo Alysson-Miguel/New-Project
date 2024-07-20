c3643808238119ab01abb42d8daf24be2f5ff429
Alysson Miguel <Edicao.Dash@gmail.com>
2024-07-19 21:27:34 -0300

    Adicao do darkmode

--- project/src/components/InputMain.tsx
+++ project/src/components/InputMain.tsx
@@ -16,39 +16,24 @@ import * as React from "react"
 export default function InputMain() {
     return (
       <>
-    <div className="flex  items-center justify-center gap-10 py-5 px-3 sm:px-6 lg:px-8">
-        <Input className="w-1/5" placeholder="Location..."  />
-        <div className="flex items-center justify-center w-3/4">
-  <div className="relative w-2/4">
-    <input
-      className="w-full py-2 px-4 rounded-lg border border-gray-300 shadow-sm focus:outline-none"
-      type="text"
-      placeholder="Search..."
-    />
-    <Image
-      className="absolute right-3 top-1/2 transform -translate-y-1/2 pointer-events-none"
-      src={"/search.svg"}
-      alt="Search"
-      width={24}
-      height={24}
-      
-    />
-  </div>
-</div>
-<Select>
-  <SelectTrigger className="w-[180px]">
-    <SelectValue placeholder="Category" />
-  </SelectTrigger>
-  <SelectContent>
-    <SelectItem value="all">all</SelectItem>
-    <SelectItem value="shirts">shirts</SelectItem>
-    <SelectItem value="mugs">mugs</SelectItem>
-    <SelectItem value="shoes">shoes</SelectItem>
-  </SelectContent>
-</Select>
+  
+      <section className="flex justify-center gap-5 mt-3" >
 
-</div>
-
+      <Input className="w-1/6"  placeholder="Location..."  />
+      <Input className="w-2/6" placeholder="Search..."  />
+  
+      <Select>
+        <SelectTrigger  className="w-[110px]" >
+          <SelectValue placeholder="Category" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="all">all</SelectItem>
+          <SelectItem value="shirts">shirts</SelectItem>
+          <SelectItem value="mugs">mugs</SelectItem>
+          <SelectItem value="shoes">shoes</SelectItem>
+        </SelectContent>
+      </Select>
+      </section>
 
 
 
--- project/src/components/navcontent.tsx
+++ project/src/components/navcontent.tsx
@@ -1,26 +1,18 @@
 "use client"
-
 import {
     Menubar,
     MenubarContent,
     MenubarItem,
     MenubarMenu,
     MenubarSeparator,
-    MenubarShortcut,
     MenubarTrigger,
   } from "@/components/ui/menubar"
-
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-
 import { ModeToggle } from "@/components/ui/theme-mode"
-import { Menu } from "lucide-react"
 
 // Para cada item eu preciso de um MenubarMenu
 export default function NavContent(){
     return(
         <>
-
-
 <Menubar className="flex justify-center">
     <ul className="flex gap-2 md:gap-5">
         <li>
@@ -56,7 +48,11 @@ export default function NavContent(){
                 </MenubarContent>
             </MenubarMenu>
         </li>
+        <li>
+    <ModeToggle/>
+    </li>
     </ul>
+
 </Menubar>
 
 
