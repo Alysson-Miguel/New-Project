e8eb6aa35316c038554586fca510791e6131de51
Alysson Miguel <Edicao.Dash@gmail.com>
2024-07-20 20:58:30 -0300

    Carrosel no meio

--- project/src/components/main.tsx
+++ project/src/components/main.tsx
@@ -14,21 +14,19 @@ import {
 export default function Main() {
     return (
         <>
-<Carousel className=" w-6/12 h-auto flex justify-center">
-  <CarouselContent >
-    <CarouselItem className=" flex justify-center py-5  h-full">
-        <Image src={ImgSale1} alt="Imagem 1" width={300} className="w-full h-auto"  />
-    </CarouselItem>
-    <CarouselItem className=" flex justify-center py-10  h-full">
-        <Image src={ImgSale2} alt="Imagem 1"  className="w-full h-auto"  />
-    </CarouselItem>
-    <CarouselItem className=" flex justify-center py-10  h-full">
-        <Image src={ImgSale3} alt="Imagem 1"  className="w-full h-auto"/>
-    </CarouselItem>
-  </CarouselContent>
-  <CarouselPrevious />
-  <CarouselNext />
-</Carousel>
+        <Carousel className="w-1/2 h-2/2 m-auto top-2">
+            <CarouselContent >
+              <CarouselItem>
+                  <Image src={ImgSale1} alt="Imagem 1" width={300} className="w-full h-auto"  />
+              </CarouselItem>
+              <CarouselItem>
+                  <Image src={ImgSale2} alt="Imagem 1"  className="w-full h-auto"  />
+              </CarouselItem>
+              <CarouselItem>
+                  <Image src={ImgSale3} alt="Imagem 1"  className="w-full h-auto"/>
+              </CarouselItem>
+            </CarouselContent>
+        </Carousel>
 
 
         </>
